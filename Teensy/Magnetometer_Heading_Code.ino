7dbe3189d44c5968eedf5580a9128ea0807d8ea5
vasas24 <vasasb@oregonstate.edu>
2019-02-17 14:55:25 -0800

    changed all instances of Wire to Wire2

--- Teensy/Magnetometer_Heading_Code.ino
+++ Teensy/Magnetometer_Heading_Code.ino
@@ -6,23 +6,23 @@ int16_t magx, magy, magz;
 
 void setup() {
   
-  Wire.begin();
+  Wire2.begin();
 
   Serial.begin(9600);
 
   //This code gets pasted into the setup section if not already there.
-  Wire.beginTransmission(Mag_addr);
-  Wire.write(0x00);
-  Wire.write(0x9C);
-  Wire.endTransmission(true);
-  Wire.beginTransmission(Mag_addr);
-  Wire.write(0x01);
-  Wire.write(0x20);
-  Wire.endTransmission(true);
-  Wire.beginTransmission(Mag_addr);
-  Wire.write(0x02);
-  Wire.write(0x00);
-  Wire.endTransmission(true);
+  Wire2.beginTransmission(Mag_addr);
+  Wire2.write(0x00);
+  Wire2.write(0x9C);
+  Wire2.endTransmission(true);
+  Wire2.beginTransmission(Mag_addr);
+  Wire2.write(0x01);
+  Wire2.write(0x20);
+  Wire2.endTransmission(true);
+  Wire2.beginTransmission(Mag_addr);
+  Wire2.write(0x02);
+  Wire2.write(0x00);
+  Wire2.endTransmission(true);
 
 }
 
@@ -38,22 +38,22 @@ void loop() {
 int heading(){
   
   //Obtain values for magnetic field across the x, y, and z axis.
-  Wire.beginTransmission(Mag_addr);
-  Wire.write(0x03);                             //Access magnetometer data register.        
-  Wire.endTransmission(false);          
-  Wire.requestFrom(Mag_addr, 2, true);          //Request 2 bytes of data from the register.
-  magx = Wire.read() << 8 | Wire.read();        //Obtain values for x magnetic field.
-  Wire.beginTransmission(Mag_addr);
-  Wire.write(0x05);                                     
-  Wire.endTransmission(false);          
-  Wire.requestFrom(Mag_addr, 2, true);
-  magz = Wire.read() << 8 | Wire.read();
-  Wire.beginTransmission(Mag_addr);
-  Wire.write(0x07);                                     
-  Wire.endTransmission(false);          
-  Wire.requestFrom(Mag_addr, 2, true);
-  magy = Wire.read() << 8 | Wire.read();
-  Wire.endTransmission(true);                   //End the transmission. 
+  Wire2.beginTransmission(Mag_addr);
+  Wire2.write(0x03);                             //Access magnetometer data register.        
+  Wire2.endTransmission(false);          
+  Wire2.requestFrom(Mag_addr, 2, true);          //Request 2 bytes of data from the register.
+  magx = Wire2.read() << 8 | Wire2.read();        //Obtain values for x magnetic field.
+  Wire2.beginTransmission(Mag_addr);
+  Wire2.write(0x05);                                     
+  Wire2.endTransmission(false);          
+  Wire2.requestFrom(Mag_addr, 2, true);
+  magz = Wire2.read() << 8 | Wire2.read();
+  Wire2.beginTransmission(Mag_addr);
+  Wire2.write(0x07);                                     
+  Wire2.endTransmission(false);          
+  Wire2.requestFrom(Mag_addr, 2, true);
+  magy = Wire2.read() << 8 | Wire2.read();
+  Wire2.endTransmission(true);                   //End the transmission. 
 
   int dir = 0;
 
